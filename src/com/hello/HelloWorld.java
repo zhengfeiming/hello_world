8b4b51eaa3a720845348f1e1532e468d8f00dc74
wangzhigang <wang_267036@163.com>
2016-03-03 11:53:08 +0800

    Initial commit

---
+++ src/com/hello/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.hello;
+
+/**
+ * Created by wzg on 2016/3/3.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("hello world!  世界 你好！");
+    }
+}
